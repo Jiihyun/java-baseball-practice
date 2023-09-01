4b6b2818132bb5273752e6dbf888720a9ed0ec2f
지현 <cwlgusc@gmail.com>
2023-09-02 02:26:56 +0900

    Feat: get input number by user

--- src/main/java/baseball4/util/UserUtil.java
+++ src/main/java/baseball4/util/UserUtil.java
@@ -1,4 +1,10 @@
 package baseball4.util;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class UserUtil {
+    //사용자로부터 세자리 수를 받는 메서드
+    public String getInputNumber() {
+        return Console.readLine();
+    }
 }
